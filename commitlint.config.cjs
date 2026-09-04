34826dbf4ba016d6dc6aea418412b16a57343f7f
Adrian Prajsnar <adrian.prajsnar11@gmail.com>
2026-09-04 22:13:02 +0200

    fix(ci): allow semantic-release commits to pass commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  // semantic-release commits include changelog lines that exceed body-max-line-length
+  ignores: [(message) => /^chore\(release\):/.test(message)],
 };
--- release.config.cjs
+++ release.config.cjs
@@ -26,7 +26,7 @@ module.exports = {
           'package-lock.json',
           'CHANGELOG.md',
         ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
     '@semantic-release/github',
